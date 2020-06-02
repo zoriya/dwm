0e6472699ec80ce4072bce46bfcbe80c9326114d
Aidan Hall <aidan.hall@outlook.com>
2020-06-02 14:41:53 +0000

    Prevents hiding the border if layout is floating.

--- dwm.c
+++ dwm.c
@@ -1849,6 +1849,14 @@ resizeclient(Client *c, int x, int y, int w, int h)
 	c->oldw = c->w; c->w = wc.width = w;
 	c->oldh = c->h; c->h = wc.height = h;
 	wc.border_width = c->bw;
+	if (((nexttiled(c->mon->cl->clients, c->mon) == c && !nexttiled(c->next, c->mon))
+		|| &monocle == c->mon->lt[c->mon->sellt]->arrange)
+		&& !c->isfullscreen && !c->isfloating
+		&& NULL != c->mon->lt[c->mon->sellt]->arrange) {
+		c->w = wc.width += c->bw * 2;
+		c->h = wc.height += c->bw * 2;
+		wc.border_width = 0;
+	}
 	XConfigureWindow(dpy, c->win, CWX|CWY|CWWidth|CWHeight|CWBorderWidth, &wc);
 	configure(c);
 	XSync(dpy, False);
