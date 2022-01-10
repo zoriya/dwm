96eadecc660079d822abd146150ed40eeaad79e2
Bakkeby <bakkeby@gmail.com>
2022-01-10 13:53:40 +0100

    togglelayout - keyboard shortcuts to set a given layout will toggle to the previous layout if the given layout is already active

--- dwm.c
+++ dwm.c
@@ -1838,9 +1838,8 @@ setfullscreen(Client *c, int fullscreen)
 void
 setlayout(const Arg *arg)
 {
-	if (!arg || !arg->v || arg->v != selmon->lt[selmon->sellt])
-		selmon->sellt ^= 1;
-	if (arg && arg->v)
+	selmon->sellt ^= 1;
+	if (arg && arg->v && arg->v != selmon->lt[selmon->sellt ^ 1])
 		selmon->lt[selmon->sellt] = (Layout *)arg->v;
 	strncpy(selmon->ltsymbol, selmon->lt[selmon->sellt]->symbol, sizeof selmon->ltsymbol);
 	if (selmon->sel)
